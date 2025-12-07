c2bb3fb4a0b0647e1733271019779cc0c7355d47
itziarayo07-alt <itziarayo07@gmail.com>
2025-12-07 11:49:09 -0600

    Soy Scrum Master incorpore el inicio de sesion para que nuestra app sea funcional, agregue algumas imagenes que usaremos

--- App.js
+++ App.js
@@ -1,22 +1,30 @@
+
 import 'react-native-gesture-handler';
+import 'react-native-reanimated';
+
+
 
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-// Solo la pantalla que SÍ tienes creada
 import Inicio from './screens/inicio';
+import Login from './screens/login';
+
 
 const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator
+      <Stack.Navigator 
         initialRouteName="Inicio"
         screenOptions={{ headerShown: false }}
       >
         <Stack.Screen name="Inicio" component={Inicio} />
+        <Stack.Screen name="Login" component={Login} />
+       
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
+
